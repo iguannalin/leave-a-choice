e7b0a613335b61ea178f40842b30c81afcc0222f
seasons986 <annalin12345@gmail.com>
2023-06-07 21:34:41 +0800

    updating for cors?

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let selected = [];
+let selected;
+let serverURL = "https://acoustic-famous-cardinal.glitch.me/";
 window.addEventListener("load", () => {
+  fetch(serverURL, { mode: "no-cors" }).then((d) => d.json()).then((r) => console.log(r));
+  selected = [];
   const sheet = document.getElementById("sheet");
   let width = 20;
   let height = 33;
@@ -7,8 +10,9 @@ window.addEventListener("load", () => {
     for (let j = 0; j < height; j++) {
       let input = document.createElement("input");
       input.type = "checkbox";
-      input.onclick = () => {
-        selected.push({i, j});
+      input.onclick = (e) => {
+        if (e.target.checked) selected.push(`(${i},${j})`);
+        else selected.splice(selected.findIndex((v) => v == `(${i},${j})`)-1, 1);
         return true;
       }
       sheet.appendChild(input);
@@ -17,5 +21,23 @@ window.addEventListener("load", () => {
 });
 
 function onSubmit() {
-  console.log('clicked', selected);
+  postData(serverURL, selected).then((data) => { 
+    console.log(data);
+  });
+}
+
+function onReset() {
+  selected = [];
+}
+
+// code from MDN (https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch)
+async function postData(url, data) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "no-cors", // no-cors, *cors, same-origin
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response;
 }
